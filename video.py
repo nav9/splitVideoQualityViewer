40265010e4eecb457b6de61666490a6ac2a099ca
Navin <navinipe@gmail.com>
2023-02-04 23:27:44 +0530

    partially done: moving video processing to separate class

--- video.py
+++ video.py
@@ -2,6 +2,22 @@ import cv2
 import time
 import numpy as np
 
+class Const:
+    FIRST_LIST_ELEMENT = 0
+    SECOND_LIST_ELEMENT = 0
+    # MOUSE_HOVER_SPLIT_MAX_VIDEO = 4 #If MAX_SIMULTANEOUS_VIDEO_DISPLAY exceeds MOUSE_HOVER_SPLIT_MAX_VIDEO, the mouse hover (which dynamically alters the video split position) capability won't be available
+    # MAX_SIMULTANEOUS_VIDEO_DISPLAY = 8 #maximum number of videos that can be shown at once (feel free to increase this number as per the hardware capability of your computer). 
+
+class VideoSplit:
+    NONE = None #There's only one video or none
+    HORIZONTAL = 0 #videos will be split horizontally. To show 6 videos, there will be 5 splits horizontally. Video split position can change on mouse hover only if there are 2 videos.
+    VERTICAL = 1 #videos will be split vertically. So to show 8 videos, there will be 7 vertical splits. Video split position can change on mouse hover only if there are 2 videos.
+    # PLUS = 'plus' #When there are 4 videos. Mouse split happens horizontally and vertically. Each video is shown in a different corner of the screen.
+    # T_UPRIGHT = 't_vertical' #When there are 3 videos and the bottom horizontal section is split into left and right halves, and the top horizontal section is shown fully
+    # T_UPSIDE_DOWN = 't_upside_down' #Opposite of T_UPRIGHT
+    # T_FALLEN_RIGHT = 't_fallen_right' #3 videos. Right vertical half is full, and the left vertical half is split horizontally
+    # T_FALLEN_LEFT = 't_fallen_left' #Opposite of T_FALLEN_RIGHT
+
 class KeyCodes:
     ESC = 27
     SPACEBAR = 32
@@ -16,59 +32,95 @@ class VideoFile:
         self.fps = self.video.get(cv2.CAP_PROP_FPS)        
         print(f"Video: {self.video}, height: {self.height}, width: {self.width}, fps: {self.fps}")
 
-#TODO: This has currently been designed to support only two videos, but can be made generic to support more
+#Note: It's not necessary that all videos will have the same dimensions, codec or framerate
+class VideoProcessor:
+    """ Performs various calculations on the videos """
+    def __init__(self, videos) -> None:
+        self.videos = videos
+        self.videoSplitType = self.determineVideoSplitType() #Whether and how a video needs to be split
+        self.allowMouseHover = False
+        self.splitLineColor = (255, 255, 255) #TODO: Draw half the line as black and half as white (or contrast it based on background pixel color)
+        self.lineThickness = 1
+        self.lineType = 8
+
+    def determineVideoSplitType(self):
+        self.videoSplitType = None
+        if len(self.videos) <= 1: self.videoSplitType = VideoSplit.NONE
+        else: self.videoSplitType = VideoSplit.VERTICAL #the default
+
+    def toggleSplitAxis(self):
+        if self.videoSplitType == VideoSplit.HORIZONTAL: self.videoSplitType = VideoSplit.VERTICAL
+        if self.videoSplitType == VideoSplit.VERTICAL: self.videoSplitType = VideoSplit.HORIZONTAL
+
+    def splitVideo(self, leftVideoFrame, rightVideoFrame, mouseX=0, mouseY=0):
+        #--- Region of Interest: Left/top side        
+        if self.videoSplitType == VideoSplit.VERTICAL: 
+            x1 = 0; y1 = 0; x2 = int(leftVideoFrame.width / 2); y2 = leftVideoFrame.height                
+        if self.videoSplitType == VideoSplit.HORIZONTAL:            
+            x1 = 0; y1 = int(leftVideoFrame.height / 2); x2 = leftVideoFrame.width; y2 = leftVideoFrame.height
+        leftRegionOfInterest = leftVideoFrame[y1:y2, x1:x2] #https://stackoverflow.com/questions/55943596/check-only-particular-portion-of-video-feed-in-opencv
+        #--- Region of Interest: Right/bottom side
+        if self.videoSplitType == VideoSplit.VERTICAL:
+            x1 = int(rightVideoFrame.width / 2); y1 = 0; x2 = rightVideoFrame.width; y2 = rightVideoFrame.height
+        if self.videoSplitType == VideoSplit.HORIZONTAL:                            
+            x1 = 0; y1 = int(rightVideoFrame.height / 2); x2 = rightVideoFrame.width; y2 = rightVideoFrame.height
+        rightRegionOfInterest = rightVideoFrame[y1:y2, x1:x2]
+        joined = np.concatenate((leftRegionOfInterest, rightRegionOfInterest), axis=self.videoSplitType)
+        #---draw the splitter line 
+        point1 = None; point2 = None
+        if self.videoSplitType == VideoSplit.VERTICAL:
+            point1 = (x1, y1); point2 = (x1, y2)
+        if self.videoSplitType == VideoSplit.HORIZONTAL:
+            point1 = (x1, y1); point2 = (x2, y1)
+        cv2.line(img=joined, pt1=point1, pt2=point2, color=self.splitLineColor, thickness=self.lineThickness, lineType=self.lineType, shift=0)
+        return joined
+
 class DisplayVideos:
     """ Provides the functionality to display the videos next to each other """
     def __init__(self, videoLocations) -> None:
         self.videos = [VideoFile(videoFile) for videoFile in videoLocations]
-        MAX_VIDEOS_MOUSE_HOVER_SUPPORTS = 4
-        self.mouseHoverSupport = True
-        if len(self.videos) > MAX_VIDEOS_MOUSE_HOVER_SUPPORTS:
-            self.mouseHoverSupport = False
         self.framerate = self.findMaxFramerate()
-        self.width, self.height = self.determineMaxDisplaySize()
+        self.width, self.height = self.findMaxDisplaySize()
         self.delay = int(self.framerate)
         #---TODO: Make video display more generic. Hardcoding now to create a basic version
-        self.leftVideo = self.videos[0]
-        self.rightVideo = self.videos[1]
+        self.leftVideo = self.videos[Const.FIRST_LIST_ELEMENT]
+        self.rightVideo = self.videos[Const.SECOND_LIST_ELEMENT]        
+        self.processor = VideoProcessor(self.videos)
 
     def display(self):
         #Concatenate images: https://stackoverflow.com/questions/7589012/combining-two-images-with-opencv
-        self.windowName = 'Comparing'
+        self.windowName = 'Comparing Videos'
         cv2.namedWindow(self.windowName, cv2.WINDOW_NORMAL)
         cv2.resizeWindow(self.windowName, self.width, self.height) 
         while self.leftVideo.video.isOpened() and self.rightVideo.video.isOpened():
-            ret_left, frame_left = self.leftVideo.video.read() #frame is a numpy nd array
-            ret_right, frame_right = self.rightVideo.video.read() 
-            if ret_left and ret_right:
-                x1 = 0; y1 = 0; x2 = int(self.leftVideo.width / 2); y2 = self.leftVideo.height                
-                regionOfInterest_left = frame_left[y1:y2, x1:x2] #https://stackoverflow.com/questions/55943596/check-only-particular-portion-of-video-feed-in-opencv
-                x1 = int(self.rightVideo.width / 2); y1 = 0; x2 = self.rightVideo.width; y2 = self.rightVideo.height
-                regionOfInterest_right = frame_right[y1:y2, x1:x2]
-                joined = np.concatenate((regionOfInterest_left, regionOfInterest_right), axis=1)
-                #---draw the vertical line TODO: Draw half the line as black and half as white (or contrast it based on background pixel color)
-                cv2.line(img=joined, pt1=(x1, y1), pt2=(x1, y2), color=(255, 255, 255), thickness=1, lineType=8, shift=0)                
+            returnValueLeft, leftVideoFrame = self.leftVideo.video.read() #frame is a numpy nd array
+            returnValueRight, rightVideoFrame = self.rightVideo.video.read() 
+            if returnValueLeft and returnValueRight:
+                joined = self.processor.splitVideo(leftVideoFrame, rightVideoFrame)
                 cv2.imshow(self.windowName, joined)       
                 keyCode = cv2.waitKey(self.delay) & 0xFF #https://stackoverflow.com/questions/57690899/how-cv2-waitkey1-0xff-ordq-works
                 if keyCode == KeyCodes.ESC:
                     break
                 if keyCode == KeyCodes.SPACEBAR:
                     time.sleep(1)
-                if keyCode == ord('h'):#to split the video horizontally
-                    pass
-            else:
+                if keyCode == ord('s') or keyCode == ord('S'):#to split the video horizontally
+                    self.processor.toggleSplitAxis()
+            else:#either the left or right frame or both could not be obtained
                 break
         self.close()
 
     def close(self):
+        """ Clear resources """
         for video in self.videos:
             video.video.release()
         cv2.destroyAllWindows()
 
-    def determineMaxDisplaySize(self):
+    def findMaxDisplaySize(self):
+        """ Returns the maximum width of any video and maximum height of any video in the list of videos """
         width = max(set([video.width for video in self.videos]))
         height = max(set([video.height for video in self.videos]))
         return width, height
 
     def findMaxFramerate(self):
+        """ Returns the maximum framerate among all videos """
         return max(set([video.fps for video in self.videos]))
